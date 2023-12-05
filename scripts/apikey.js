c53e6c88a080649b6ce3d6ecdef003a6ee8acd53
Aidan Younathan <ayounathan05@gmail.com>
2023-12-05 13:59:33 -0800

    Did most of the search just need to move it

--- scripts/apikey.js
+++ scripts/apikey.js
@@ -1,4 +1,4 @@
 export { apiKey }
 
 
-let apiKey = "46d308519b71963a9ac9b27f5625737a"
+let apiKey = "46d308519b71963a9ac9b27f5625737aJJJJJJJJJJJ"
--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,19 @@ import { apiKey } from "./apikey.js";
 
 //calling the daily weather
 async function dailyWeather() {
-    const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Modesto,california&units=imperial&appid=${apiKey}`);
+    console.log(apiKey);
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`);
     const data = await promise.json()
     console.log(data);
 }
 dailyWeather()
 
 //Calling the five day forecast
-async function fiveDay() {
-    const promise2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Modesto, California&appid=${apiKey}`);
-    const data2 = await promise2.json()
-    console.log(data2);
-}
+// async function fiveDay() {
+//     const promise2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Modesto,California&appid=${apiKey}`);
+//     const data2 = await promise2.json()
+//     console.log(data2);
+// }
 
-fiveDay()
+// fiveDay()
 
