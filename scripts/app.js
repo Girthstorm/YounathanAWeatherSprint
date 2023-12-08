b24047e190e592ee13b50e0bfa4b082c917d8aa3
Aidan Younathan <ayounathan05@gmail.com>
2023-12-08 15:23:20 -0800

    ALmost There

--- scripts/app.js
+++ scripts/app.js
@@ -19,6 +19,11 @@ let windUnits = document.getElementById("windUnits");
 let mphms = "MPH";
 let favStr = document.getElementById("favStr");
 let favors = [];
+let currentCity;
+let currentState;
+let searchBtn = document.getElementById("searchBtn");
+let dates = [];
+let newSearch;
 
 
 function unitcheck() {
@@ -66,26 +71,27 @@ function stateNameToAbbreviation(fullStateName) {
         case 'Montana': return 'MT';
         case 'Nebraska': return 'NE';
         case 'Nevada': return 'NV';
-        case 'New hampshire': return 'NH';
-        case 'New jersey': return 'NJ';
-        case 'New mexico': return 'NM';
+        case 'New Hampshire': return 'NH';
+        case 'New Jersey': return 'NJ';
+        case 'New Mexico': return 'NM';
+        case 'New York': return 'NY';
         case 'New york': return 'NY';
-        case 'North carolina': return 'NC';
-        case 'North dakota': return 'ND';
+        case 'North Carolina': return 'NC';
+        case 'North Dakota': return 'ND';
         case 'Ohio': return 'OH';
         case 'Oklahoma': return 'OK';
         case 'Oregon': return 'OR';
         case 'Pennsylvania': return 'PA';
-        case 'Rhode island': return 'RI';
-        case 'South carolina': return 'SC';
-        case 'South dakota': return 'SD';
+        case 'Rhode Island': return 'RI';
+        case 'South Carolina': return 'SC';
+        case 'South Dakota': return 'SD';
         case 'Tennessee': return 'TN';
         case 'Texas': return 'TX';
         case 'Utah': return 'UT';
         case 'Vermont': return 'VT';
         case 'Virginia': return 'VA';
         case 'Washington': return 'WA';
-        case 'West virginia': return 'WV';
+        case 'West Virginia': return 'WV';
         case 'Wisconsin': return 'WI';
         case 'Wyoming': return 'WY';
         default: return null;
@@ -106,6 +112,8 @@ async function loadWeather() {
     console.log(fullState);
     const TOABR = stateNameToAbbreviation(fullState)
     city.innerText = `${data4[0].name}, ${TOABR}`;
+    currentCity = data4[0].name;
+    currentState = data4[0].state;
     weatherCond.innerText = (data3.weather[0].main)
     unitcheck()
     windUnits.innerText = mphms;
@@ -113,7 +121,25 @@ async function loadWeather() {
     minTemp.innerText = `${Math.trunc(data3.main.temp_min)}${ForC}`
     windSpeed.innerText = Math.trunc(data3.wind.speed)
     humidity.innerText = data3.main.humidity
+
+    const promise6 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`)
+    const data6 = await promise6.json();
+    console.log(data6);
+    checkIfFavorites()
+    if(localStorage.getItem('City') != undefined){
+        favors = JSON.parse(localStorage.getItem('City'));
+    }
 }
+
+function checkIfFavorites(){
+    if(favors.includes(`${currentCity}, ${currentState}`)) {
+        favStr.src = "./assets/star-fill-copy.svg"
+        console.log("yipee");
+    } else {
+        favStr.src = "./assets/star.svg"
+    }
+}
+checkIfFavorites()
 
 //Farenheit or however you spell that
 Far.addEventListener('click', function(event){
@@ -135,29 +161,23 @@ Cel.addEventListener('click', function(event){
 console.log(favors)
 
 favStr.addEventListener('click', function(event){
-    
-    
-    if (favors.includes(city.innerText)){
+    if (favors.includes(`${currentCity}, ${currentState}`)){
         favStr.src = "./assets/star.svg"
-        let nameIndex = favors.indexOf(city.innerText);
+        let nameIndex = favors.indexOf(`${currentCity}, ${currentState}`);
         favors.splice(nameIndex , 1)
         console.log(favors);
+        
     } else {
-        favors.push(city.innerText);
-        favStr.src = "./assets/star-fill.svg"
+        favors.push(`${currentCity}, ${currentState}`);
+        favStr.src = "./assets/star-fill-copy.svg"
+        localStorage.setItem('City', JSON.stringify(favors))
         console.log(favors);
     }
 })
 
 
-//calling the daily weather
-async function dailyWeather() {
-    console.log(apiKey);
-    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=${unit}`);
-    const data = await promise.json()
-    console.log(data);
-}
-dailyWeather()
+
+
 
 // set date get date 
 //Calling the five day forecast
@@ -168,6 +188,8 @@ dailyWeather()
 // }
 
 // fiveDay()
+
+
 
 navigator.geolocation.getCurrentPosition(success, errorFunc);
 
@@ -182,3 +204,55 @@ function errorFunc(error) {
     console.log(error.message)
 }
 
+async function Search() {
+    newSearch = search.value;
+    console.log(newSearch);
+    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${newSearch}&appid=${apiKey}&units=${unit}`);
+    console.log(`https://api.openweathermap.org/data/2.5/weather?q=${newSearch}&appid=${apiKey}&units=${unit}`);
+    const data = await promise.json()
+    console.log(data);
+    lat = data.coord.lat
+    lon = data.coord.lon
+
+    Temp.innerText = Math.trunc(data.main.temp);
+    const promise5 = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`)
+    const data5 = await promise5.json();
+    console.log(data5);
+    fullState = data5[0].state;
+    console.log(fullState);
+    const TOABR = stateNameToAbbreviation(fullState)
+    city.innerText = `${data5[0].name}, ${TOABR}`;
+    currentCity = data5[0].name;
+    currentState = data5[0].state;
+    weatherCond.innerText = (data.weather[0].main)
+    unitcheck()
+    windUnits.innerText = mphms;
+    maxTemp.innerText = `${Math.trunc(data.main.temp_max)}${ForC}`
+    minTemp.innerText = `${Math.trunc(data.main.temp_min)}${ForC}`
+    windSpeed.innerText = Math.trunc(data.wind.speed)
+    humidity.innerText = data.main.humidity;
+    fiveDayFetch()
+    checkIfFavorites()
+}
+
+searchBtn.addEventListener('click', function(event){
+    Search();
+})
+async function fiveDayFetch() {
+    const promise6 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${newSearch}&appid=${apiKey}&units=${unit}`)
+    const data6 = await promise6.json();
+    console.log(data6);
+}
+
+
+//Ellie helped me a lot with the five day, credit to her :)
+async function fiveDay() {
+    let date = new Date();
+    for(let i = 0; i < dates.length; i++){
+        date.setDate(now.getDate()+1);
+        let n = date.getDate() // Day of week
+        
+    }
+    
+}
+fiveDay()
